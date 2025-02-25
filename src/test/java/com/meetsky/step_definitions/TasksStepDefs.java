6624706f41a9a0c5fb2a84fd14ca641ae5bb831d
Afilya <adnanfilya24@gmail.com>
2025-02-25 09:05:09 +0300

    I completed all TCs.

--- src/test/java/com/meetsky/step_definitions/TasksStepDefs.java
+++ src/test/java/com/meetsky/step_definitions/TasksStepDefs.java
@@ -25,24 +25,70 @@ public class TasksStepDefs {
     public void user_clicks_add_list() {
         tasksPage.addList.click();
         BrowserUtils.waitFor(2);
-
     }
 
     @When("user writes {string} for the list and clicks submit button")
     public void user_writes_the_list_and_clicks_submit_button(String listName) {
-        tasksPage.listName.sendKeys(listName);
+        tasksPage.newListName.sendKeys(listName);
         tasksPage.submitButton.click();
-
     }
 
     @Then("user should see the {string} list under Tasks label")
     public void user_should_see_the_list_under_tasks_label(String expectedStr) {
-
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(2));
         wait.until(ExpectedConditions.urlContains(expectedStr.toLowerCase()));
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedStr.toLowerCase()));
-
     }
 
+    @When("user clicks {string} list")
+    public void user_clicks_list(String list) {
+        tasksPage.selectList(list);
+    }
 
+    @When("user writes {string} for the task and presses Enter button")
+    public void user_writes_for_the_task_and_presses_enter_button(String taskName) {
+        tasksPage.addTask.sendKeys(taskName + Keys.ENTER);
+    }
+
+    @Then("user should see the {string} as a task")
+    public void user_should_see_it_as_a_task(String expectedTask) {
+        String actualTask = tasksPage.taskControl(expectedTask).getText();
+        Assert.assertEquals(expectedTask, actualTask);
+    }
+
+    @When("user clicks checkbox near {string} task")
+    public void user_clicks_checkbox_near_task(String checkboxName) {
+        tasksPage.checkbox(checkboxName);
+    }
+
+    @Then("user should see {string} in completed tasks")
+    public void user_should_see_in_completed_tasks(String expectedTask) {
+        tasksPage.completedTasks.click();
+        String actualTask = tasksPage.taskControl(expectedTask).getText();
+        Assert.assertEquals(expectedTask, actualTask);
+    }
+
+    @When("user clicks the star icon of {string} task")
+    public void user_clicks_the_star_icon_of_task(String importantTask) {
+        tasksPage.starIcon(importantTask);
+    }
+
+    @Then("user should see {string} in important tasks")
+    public void user_should_see_in_important_tasks(String expectedTask) {
+        tasksPage.importantTasks.click();
+        Assert.assertEquals(expectedTask, tasksPage.taskControl(expectedTask).getText());
+    }
+
+    @When("user clicks the Current tab")
+    public void user_clicks_the_current_tab() {
+        tasksPage.unCompletedTasks.click();
+    }
+
+    @Then("user should see the number of all uncompleted tasks")
+    public void user_should_see_the_number_of_all_uncompleted_tasks() {
+        String actualTask = tasksPage.numberOfUnCompletedTasks.getText();
+        String expectedTask = tasksPage.numberOfAllTasks.getText();
+        Assert.assertEquals(expectedTask, actualTask);
+
+    }
 }
